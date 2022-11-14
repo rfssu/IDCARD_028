277cf8951974ba3396ba23ff5687580dcfa5c2f7
rfssu <rafi.saifullah.ft20@mail.umy.ac.id>
2022-11-14 20:26:04 +0700

    Initial Commit

---
+++ src/test/java/pws/takehome/idcard/IdcardApplicationTests.java
@@ -0,0 +1,13 @@
+package pws.takehome.idcard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdcardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
